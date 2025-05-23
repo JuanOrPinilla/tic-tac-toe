020f8825918ab24fc4a6f680ec0b1787a70d7fb3
Juan David Orduz Pinilla <j.orduz@uniandes.edu.co>
2025-05-23 02:27:25 -0500

    j2 text

--- src/components/J2/index.js
+++ src/components/J2/index.js
@@ -5,7 +5,7 @@ export default class J2 extends React.Component {
   render() {
     return (
       <div className='J2'>
-        <h1 className='h1-bg'>X (J1)</h1>
+        <h1 className='h1-bg'>X (J2)</h1>
         <h2 className='h2-bg'>10</h2>
       </div>
     );
